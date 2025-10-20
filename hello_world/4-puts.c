315ca9c4a5828294c2cbcf4c9bcb85c01a5ead06
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-20 09:11:02 +0000

    fix: remove whitespace

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 
 /**
-* main-prints "Programming is like building a multilingual 
-* puzzle, followed by a new line." 
+* main-prints "Programming is like building a multilingual
+* puzzle, followed by a new line."
 * return 0.
 */
 
