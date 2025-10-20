31e2cb6e98691c55d66573d3d39ac139a3397065
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-20 09:07:42 +0000

    fix: add ";" to return

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
-* main- prints "Programming is like building a multilingual 
+* main - prints "Programming is like building a multilingual 
 * puzzle, followed by a new line." 
 * return 0.
 */
@@ -11,7 +11,6 @@ int main(void)
 
 {
 puts("\"Programming is like building a multilingual puzzle");
-
-return(0)
+return(0);
 }
 
