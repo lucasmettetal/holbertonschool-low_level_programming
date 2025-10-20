5fb730ea27d5cfdfacf45e4675e9e00010b511d3
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-20 09:29:33 +0000

    fix: change printf

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,13 +9,13 @@
 
 int main (void)
 {
- // Use %zu for size_t if your compiler supports it.
- printf("sizeof(int)       = %d\n",sizeof(int));
- printf("sizeof(float)     = %d\n",sizeof(float));
- printf("sizeof(double)    = %d\n",sizeof(double));
- printf("sizeof(char)      = %d\n",sizeof(char));
- printf("sizeof(167)       = %d\n",sizeof(167));
- printf("sizeof(3.1415926) = %d\n",sizeof(3.1415926));
- printf("sizeof('$')       = %d\n",sizeof('$'));
- return 0;
+// Use %zu for size_t if your compiler supports it.
+printf("Size of integer: %ul",sizeof(int));
+printf("Size of float: %ul",sizeof(float));
+printf("Size of double: %ul",sizeof(double));
+printf("Size of char: %ul",sizeof(char));
+printf("Size of 167: %ul",sizeof (167));
+printf("Size of 3.1415926: %ul",sizeof(3.1415926));
+printf("Size of integer: %ul",sizeof(‘$’));
+return 0;
 }
