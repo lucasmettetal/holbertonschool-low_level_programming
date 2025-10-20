40ae69c84d619db3be97a145169f40ad34f04b18
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-20 09:38:17 +0000

    fix: fix wrong syntaxe

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,13 +7,13 @@
 */
 
 
-int main (void)
+int main(void)
 {
-// Use %zu for size_t if your compiler supports it.
+
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
-return 0;
+return (0);
 }
