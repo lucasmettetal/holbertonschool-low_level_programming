819caac74e09e93dcab1d38d1f01f8d792175dce
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-20 09:36:34 +0000

    fix: change printf again

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,12 +10,10 @@
 int main (void)
 {
 // Use %zu for size_t if your compiler supports it.
-printf("Size of integer: %ul",sizeof(int));
-printf("Size of float: %ul",sizeof(float));
-printf("Size of double: %ul",sizeof(double));
-printf("Size of char: %ul",sizeof(char));
-printf("Size of 167: %ul",sizeof (167));
-printf("Size of 3.1415926: %ul",sizeof(3.1415926));
-printf("Size of integer: %ul",sizeof(‘$’));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return 0;
 }
