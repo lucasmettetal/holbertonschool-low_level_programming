a4f8f73c224cc015ec34c2f27d40c564feac46d9
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 13:14:05 +0000

    feat: put number 1 to 9 only putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - put a number 1 to 9
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+char x = 0;
+while (x < 10)
+x++;
+
+{
+printf("%d", x);
+
+if (x < 9);
+
+}
+putchar('\n');
+
+return (0);
+
+}
