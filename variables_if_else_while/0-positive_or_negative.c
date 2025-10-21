c8321c0dfa9194bda6075df1ce79d8a29673af76
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 07:19:36 +0000

    feat: put positif or negatif is the number is

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Return positif ou négatif
+*
+* Return: Always 0
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("%d \n", x);
+/* your code goes there */
+if (x > 0)
+{
+printf("is positive \n");
+}
+else if (x < 0)
+{
+printf("is negative \n");
+}
+return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+variables_if_else_while 2025/10/20
