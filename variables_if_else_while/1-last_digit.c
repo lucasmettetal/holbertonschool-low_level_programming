a9b66877fcdfc840c1cf0455e1644b2e4791595e
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 08:38:32 +0000

    feat: check the last number and return condition

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Return the last number and compare
+*
+* Return: Always 0
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n, y;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+/* your code goes there */
+y = n % 10;
+printf(" Last digit of %d is %d ", n, y);
+if (n < 6 && n != 0)
+{
+printf(" and is less than 6 and not 0\n");
+}
+else if (n > 5)
+{
+printf(" is greater than 5\n");
+}
+else if (n = 0)
+{
+printf(" and is 0\n");
+}
+
+
+return (0);
+}
