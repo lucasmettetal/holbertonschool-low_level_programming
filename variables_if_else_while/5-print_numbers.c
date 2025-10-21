243edf8de2ac614bc7fc88769668fd4f2c5becf5
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 13:10:21 +0000

    feat: put number 1 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - put a number 1 to 9
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int x = 0;
+while (x < 10)
+
+
+{
+printf("%d", x);
+x++;
+
+}
+
+printf("\n");
+return (0);
+
+}
