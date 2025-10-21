cd7ae38c8ad0e73d43f849f9c89c52dcee6f8d39
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 13:42:47 +0000

    feat: rints all possible combinations of single-digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - put a number 1 to 9
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int x = 0;
+
+while (x < 10)
+{
+putchar(x + '0');
+if (x < 9)
+{
+putchar(',');
+putchar(' ');
+}
+x++;
+}
+putchar('\n');
+
+return (0);
+
+}
