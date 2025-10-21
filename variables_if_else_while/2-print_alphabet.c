699e7362592d923530a24625f234e99919b7d936
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 08:59:48 +0000

    feat: putchar alphabet a to z

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* Declare a variable and initialize it with the first member of 
+*the range [a - z] 
+*/
+
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+
+}
+putchar('\n');
+return(0);
+}
