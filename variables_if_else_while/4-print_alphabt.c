1686fb8bf4945c5f5986937e51505f5f1b1ea5f6
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 12:44:09 +0000

    fix: "if"

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,12 @@ int main(void)
 {
 char alphabet = 'a';
 while (alphabet <= 'z')
-if (alphabet == 'e'||alphabet == 'q')
-
+{
+if (alphabet != 'e' && alphabet != 'q')
 {
 putchar(alphabet);
+}
 alphabet++;
-continue;
 
 }
 putchar('\n');
