a4aacfa04322bb4087fd57f7814536ebdf33167d
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 08:52:15 +0000

    fix: change n to y

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 y = n % 10;
 printf("Last digit of %d is %d", n, y);
-if (n < 6 && n != 0)
+if (y < 6 && n != 0)
 {
 printf(" and is less than 6 and not 0\n");
 }
-else if (n > 5 && n != 0)
+else if (y > 5)
 {
 printf(" and is greater than 5\n");
 }
