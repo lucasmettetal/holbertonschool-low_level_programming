f9893ded508a36399fb4824a1cde52e94b6041d6
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 09:42:17 +0000

    feat: prints alphabet un lower and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - print alphabet lowcase en uppercase
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+alphabet = 'A'
+while (alphabet <= 'Z')
+{
+putchar(alphabet)
+alphabet++;
+}
+
+putchar('\n');
+return (0);
+}
