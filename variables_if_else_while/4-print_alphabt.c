5fbd1fd0a3b3140f8e9bfffdb055a6fabe18ace2
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-21 12:27:53 +0000

    feat: put alphabet in lowcase but not "e" and "q"

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - put alphabet in lowcase but not "e" and "q"
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+if (aphabet == 'e'||aphabet == 'q')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
