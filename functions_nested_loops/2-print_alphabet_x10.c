df6af69899b65b3201ff012bc9acd599d6584aa3
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-22 12:34:11 +0000

    feat: prints 10 times the alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Prints the alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{
+char alphabet = 'a';
+while (alphabet < 5)
+
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
+}
+_putchar('\n');
+
+}
