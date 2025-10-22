8897ef7c8e41137d4b2c104531ef5533013ebb72
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-22 09:07:23 +0000

    fix: betty fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,23 +2,23 @@
 
 
 /**
- * main - Prints _putchar, followed by a new line
- *
- * Return: Always 0
- */
+* main - Prints _putchar, followed by a new line
+*
+* Return: Always 0
+*/
 
 int main(void)
 
 {
 
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
