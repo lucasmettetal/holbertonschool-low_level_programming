aec0bad4e79122fadb183622fe61f1a80663f53a
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-23 12:11:18 +0000

    feat: a function to prints the sign of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - return 1 if greater zero, return 0 if is 0; return -1 less zero
+ * @n: The character to check
+ * Return: 1 if c is lowercase and uppercase, 0 otherwise
+ */
+
+int print_sign(int n);
+{
+	if (n > 0 && n != 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+}
