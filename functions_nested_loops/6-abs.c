9d6b79c128022a2e7bf373b5d9da7cd714ae23cc
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-23 13:19:16 +0000

    feat: ceate function abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - return that computes the absolute value of an integer
+ * @n: The character to check
+ * Return: the absolute value of n
+ */
+
+int _abs(int n)
+{
+
+	if (N < 0)
+		return (-n) else
+
+			return (n);
+}
