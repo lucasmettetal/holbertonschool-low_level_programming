b1c726598ee74ac9c78550bb1c64e31f1652a9b6
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-24 09:16:46 +0000

    feat: write function that prints all natural numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98
+ *@n: list of natural number
+ *Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+
+	if (n <= 98)
+	{
+
+		for (int i = n; i <= 98; i++)
+		{
+
+			_putchar(i + '0');
+
+			if (i != 98)
+			{
+
+				_putchar(",");
+				_putchar(" ");
+			}
+		}
+	}
+	else
+	{
+
+		for (int i = n; i >= 98; i--)
+		{
+
+			_putchar(i + '0');
+
+			if (i != 98)
+			{
+				_putchar(",");
+				_putchar(" ");
+			}
+		}
+	}
+
+	_putchar("\n");
+}
