f7824a7f75d6cc0f67f80b8acec82f8aa8707053
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-24 07:36:17 +0000

    fix: fix syntaxe error

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,10 +8,11 @@
 void times_table(void)
 {
 	int result;
+	int j, i;
 
-	for (i = 0, i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0, j <= 9, j++)
+		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
 			_putchar(result);
