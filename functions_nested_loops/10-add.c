b56781dae7b21065e74f320a17db2b8fff8d6138
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-24 08:53:59 +0000

    feat: create function for return the result of to integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *@a: first number
+ *@b: second number
+ *Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+
+	int tint = add(a, b);
+
+	_putchar(tint + '0');
+
+	return (0);
+}
+{
+	return (a + b);
+}
