167a797eb36a93469859072b44e0864c91ec7e18
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-24 07:44:20 +0000

    fix: remove int and replace to char

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 	char result;
 	char j, i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
 			_putchar(result);
