277c3b99185750964ca2faad9f3c13ec2dc2026c
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-27 08:44:27 +0000

    feat: function for multiplies two integers.

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - checks if 'c' is digit or not
+ *@a: first number
+ *@b: second number
+ * Return: 1 if c is lowercase and uppercase, 0 otherwise
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
