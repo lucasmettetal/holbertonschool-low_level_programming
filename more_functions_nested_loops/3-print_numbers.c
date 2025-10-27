3c1b01239aa4e194e13be4b8be8cead9363bb314
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-27 09:02:48 +0000

    feat: Write a function that prints the numbers, from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
