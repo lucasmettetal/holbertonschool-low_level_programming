868cae233b01353704ca9194111c2ed987caa6aa
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-27 10:10:41 +0000

    feat: a function for draws a straight line in the terminal.

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n : The character to check
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int l;
+
+	for (l = 0; l <= n; l++)
+
+	{
+
+		if (num < 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}
