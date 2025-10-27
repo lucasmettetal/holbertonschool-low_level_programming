9d4bf0d6f7af97186822471a457268e61ade4f70
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-27 09:22:23 +0000

    feat: prints 10 times the numbers, from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,26 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9
+ * more_numbers - prints the numbers, from 0 to 14, 10 time
  * Return: 0
  */
 
 void more_numbers(void)
+{
+	int count = 0;
+	char num;
+
+	while (count < 10)
+
+	{
+		num = '0';
+
+		while (num <= '14')
+		{
+			_putchar(num);
+			num++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
