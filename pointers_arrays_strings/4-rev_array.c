1d6be14e9de80306f95bfb8e4367e2f6070bff29
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-30 14:15:59 +0000

    fix: correct array reversal logic in reverse_array function

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,8 +12,13 @@ void reverse_array(int *a, int n)
 
 	int temp;
 
+	if (n <= 0)
+		return;
+
 	for (i = 0; i < n / 2; i++)
+	{
 		temp = a[1];
-	a[i] = a[n - 1 - i];
-	a[n - 1 - i] = temp;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
 }
