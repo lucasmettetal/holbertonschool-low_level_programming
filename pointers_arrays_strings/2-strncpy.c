9e74bf7c68b975e67aee78de92a6088152c0e136
lucasmettetal <lucas.mettetal2@gmail.com>
2025-10-30 13:51:19 +0000

    feat: add _strncpy function to copy a string with specified limit

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - function to copy the contents of one string into another
+ * @dest: the buffer pointed
+ * @src: the string
+ * @n: nombre of letter to prints
+ * Return: the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i++] = '\0';
+	}
+	return (dest);
+}
