1d7aa096f152ec3af64cd6b53839f6b007a9de09
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-03 20:16:04 +0000

    fix: correct logic and formatting issues in _strspn function

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - calculates the length of a prefix substring
+ * @s: string to search
+ * @accept: string containing accepted characters
+ * Return: number of bytes in the initial segment of s
+ *         which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0,
+				 int i, found;
+
+	while (s[count])
+	{
+		found = 0
+
+			for (i = 0; accept[i]; i++)
+		{
+			if (s[count] == accept[i])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+		{
+			break;
+			count++;
+		}
+	}
+	return (count);
+}
