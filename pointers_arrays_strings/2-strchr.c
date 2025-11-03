cd51b3c6b6f0b17dded6da057a31fd3f90f9b33f
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-03 10:28:54 +0000

    feat: implement _strchr function to locate a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to locate
+ * Return: pointer to first occurrence of c in s, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+}
