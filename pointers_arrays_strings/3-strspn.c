7bed079deb56490febe6564d0c8793fd1ccd2f90
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-03 20:39:12 +0000

    fix: correct loop condition checks in _strspn function

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -13,11 +13,11 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int count = 0;
 	int i, found;
 
-	while (s[count])
+	while (s[count] != '\0')
 	{
 		found = 0;
 
-		for (i = 0; accept[i]; i++)
+		for (i = 0; accept[i] != '\0'; i++)
 		{
 			if (s[count] == accept[i])
 			{
