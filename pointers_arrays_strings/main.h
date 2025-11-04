11867d5015ed210786880c2fd42aa0f1f9a813b4
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-04 09:09:21 +0000

    feat: add print_chessboard to prints the chessboard

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,4 +22,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 #endif
