ca5287e16c8cd6b021938122feee98dc6680e731
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-05 10:47:58 +0000

    feat: Add 6-is_prime_number.c: implement recursive prime number checker

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * check_prime - checks if a number is divisible by any smaller number
+ * @n: number to check
+ * @i: current divisor
+ * Return: 1 if prime, 0 if not
+ */
+
+int check_prime(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (check_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if n is a prime number, otherwise 0
+ * @n: number to check
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, 2));
+}
