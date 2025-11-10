c072a07f7800c0aedb958847275fb60fac3cc7f5
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-10 13:24:59 +0000

    feat: add a fonction to creates an array of chars

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c: char to initialize the array
+ *
+ * Return: pointer to the array, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+	if (size == 0)
+		return (NULL);
+	array = (char *)malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
+}
