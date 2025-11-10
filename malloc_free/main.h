32dc1fd9f40f975fb1ab6f61b9965db913e759be
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-10 15:57:20 +0000

    feat: add a function to allocates a 2D array of integers

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -28,4 +28,5 @@ void _puts_recursion(char *s);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 #endif
