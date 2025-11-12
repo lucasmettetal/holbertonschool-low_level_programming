ebbcdfeca2740e666c3b45e3f8edba3133628899
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-12 12:57:54 +0000

    feat: create a function to allocates memory for an array of nmemb elements of size bytes

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array of nmemb elements of size bytes
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: pointer to allocated memory, or NULL on failure
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, total;
+	void *ptr;
+	unsigned char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total = (size_t)*nmemb(size_t) * size;
+	if (nmemb != 0 && total / nmemb != size)
+		return (NULL);
+
+	ptr = malloc(total);
+	if (ptr == NULL)
+		return (NULL);
+
+	p = (unsigned char *)ptr;
+	for (i = 0; i < total; i++)
+
+		p[i] = 0;
+
+	return (ptr);
+}
