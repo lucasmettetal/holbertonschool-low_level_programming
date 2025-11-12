998aab33acd4d604188a15476a486a807a4567fb
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-12 20:51:53 +0000

    feat: add a function to prints a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog to print
+ * Return: 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("Dog is NULL\n");
+		return;
+	}
+
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+}
