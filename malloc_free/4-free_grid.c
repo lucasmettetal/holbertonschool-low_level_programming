d81ced27e2ce9e9c0e04e38d00d314737f4d12bb
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-12 10:18:57 +0000

    feat: create a fonction to allocate memory to grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - allocate memory to grid
+ *
+ * @grid: int pointer
+ *
+ * @height: int arg
+ *
+ * Return: grid of 0s
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; ++i)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
