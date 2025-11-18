14d17bbadebc1859af68748d2a7887baf5cc06a9
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-18 09:45:37 +0000

    fix: create the true files

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,23 +2,18 @@
 #include <stddef.h>
 
 /**
- * int_index - searches for an integer
- * @array: array of integers
+ * array_iterator - executes a function on each element of an array
+ * @array: pointer to the array
  * @size: size of the array
- * @cmp: pointer to the function to be used to compare values
+ * @action: function pointer to the function to execute
  * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	size_t i;
 
-	if (array == NULL || cmp == NULL || size <= 0)
-		return (-1);
-
+	if (array == NULL || action == NULL)
+		return;
 	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
-	}
-	return (-1);
+		action(array[i]);
 }
