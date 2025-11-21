2fde4dc4379862dcb99047e5e28854abeba9d1b7
lucasmettetal <lucas.mettetal2@gmail.com>
2025-11-21 10:07:15 +0000

    fix: syntaxe error

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,20 +11,20 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i, ap;
+	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(ap, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(ap, int);
+		sum += va_arg(args, int);
 	}
 
-	va_end(ap);
+	va_end(args);
 
 	return (sum);
 }
