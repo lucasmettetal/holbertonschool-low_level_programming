2a1c976417bf4d0b4a7f27b05cc0b96360fbe096
lucasmettetal <lucas.mettetal2@gmail.com>
2025-12-01 14:43:04 +0000

    fix: reorder include directives in lists.h for consistency

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stddef.h>
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 size_t print_list(const list_t *h);
 int _putchar(char c);
