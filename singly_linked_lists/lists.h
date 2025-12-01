0087e4237c86cad4c6bdee7f982c58b7b001853f
lucasmettetal <lucas.mettetal2@gmail.com>
2025-12-01 14:46:35 +0000

    fix: add struct list_s in lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,8 +5,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ 
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 
 size_t print_list(const list_t *h);
-int _putchar(char c);
+
 
 #endif
