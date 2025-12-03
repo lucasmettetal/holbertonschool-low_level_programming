f2b3320047f63d90a73fb4a6ecdc5ce1458b9ae1
lucasmettetal <lucas.mettetal2@gmail.com>
2025-12-03 14:23:44 +0000

    fix: correct formatting and structure definitions in print_dlistint and lists.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,32 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOUBLY_LINKED_LISTS_H
+#define DOUBLY_LINKED_LISTS_H
 
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string
- * @len: length of the string
+ * struct dlistint_s - doubly linked list node
+ * @n: integer stored in the node
+ * @prev: points to the previous node
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: doubly linked list node structure
  */
-
-typedef struct list_s
+typedef struct dlistint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
 size_t print_dlistint(const dlistint_t *h);
 
-#endif
+#endif /* DOUBLY_LINKED_LISTS_H */
