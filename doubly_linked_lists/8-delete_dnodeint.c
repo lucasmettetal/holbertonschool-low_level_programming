d0bde5345213ec75760b652056afcbf36a4b4d53
lucasmettetal <lucas.mettetal2@gmail.com>
2025-12-05 09:36:52 +0000

    fix: reorder pointer updates in delete_dnodeint_at_index for correct node deletion

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -32,11 +32,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (current == NULL)
 		return (-1);
 
-	if (current->next != NULL)
-		current->next->prev = current->prev;
-
 	if (current->prev != NULL)
 		current->prev->next = current->next;
+
+	if (current->next != NULL)
+		current->next->prev = current->prev;
 
 	free(current);
 	return (1);
