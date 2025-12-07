58107762acece52aea3332e806e89b2c28714892
lucasmettetal <lucas.mettetal2@gmail.com>
2025-12-07 23:10:47 +0000

    feat: implement hash table creation function and add header file

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,12 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
